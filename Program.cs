d6ace5beb4d0fc530f0149db0215c3ea269abec8
aracaraujo <araaraujo.07@gmail.com>
2022-01-26 22:59:39 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Hilo.Game;
+
+
+namespace Hilo
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            Dealer dealer = new Dealer();
+            dealer.StartGame();
+        }
+    }
+}
